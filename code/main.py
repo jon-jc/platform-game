6b6730ecf5bae8acacb0aca8fec9ee2924aab61f
jon-jc <jonathancho.jc@gmail.com>
2024-04-30 17:49:16 -0700

    Refactor code base, add level assets, create basic level layout

--- code/main.py
+++ code/main.py
@@ -1,10 +1,18 @@
 from settings import *
+from level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Souls Like Platformer')
+
+        self.tmx_maps = {0: load_pygame(join('.', 'data', 'levels', 'omni.tmx'))}
+        self.current_stage = Level(self.tmx_maps[0] )
+
+    
 
 
     def run(self):
@@ -13,6 +21,8 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+            self.current_stage.run()
 
             pygame.display.update()
 
