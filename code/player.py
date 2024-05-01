79852561d4214c621ac1730b39768d3659a4c67f
jon-jc <jonathancho.jc@gmail.com>
2024-05-01 10:10:38 -0700

    Add gravity to Player class for vertical movement

--- code/player.py
+++ code/player.py
@@ -13,6 +13,7 @@ class Player(pygame.sprite.Sprite):
         #movement
         self.direction = vector()
         self.speed = 250
+        self.gravity = 1000
         #collision
         self.collision_sprites = collision_sprites
         
@@ -29,12 +30,18 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_d]:
             input_vector.x = 1
         
-        self.direction = input_vector.normalize() if input_vector else input_vector
+        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
        
     def move(self, dt):
+        #horizontal
         self.rect.x += self.direction.x * self.speed * dt
         self.collision('horizontal')
-        self.rect.y == self.direction.y * self.speed * dt
+
+        #vertical
+        self.direction.y += self.gravity / 2 * dt
+
+        self.rect.y += self.direction.y * dt
+        self.direction.y += self.gravity / 2 * dt
         self.collision('vertical')
 
     def collision(self, axis):
@@ -48,7 +55,15 @@ class Player(pygame.sprite.Sprite):
                     if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
                         self.rect.right = sprite.rect.left
                 else:
-                    pass
+                    #top
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+
+                    #bottom
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top
+                    self.direction.y = 0
+                
 
     def update(self, dt):
         self.old_rect = self.rect.copy()
