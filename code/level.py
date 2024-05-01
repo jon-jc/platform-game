60c831c1672409cf8804b168bd3ecca54f8f3898
jon-jc <jonathancho.jc@gmail.com>
2024-05-01 10:01:55 -0700

    Add collision detection for player object in Level class

--- code/level.py
+++ code/level.py
@@ -11,6 +11,8 @@ class Level:
 
 #groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+
 
         self.setup(tmx_map)
 
@@ -18,10 +20,10 @@ class Level:
 
     def setup(self, tmx_map):
         for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites))
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
-                Player((obj.x, obj.y), self.all_sprites)
+                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
     def run(self, dt):
         self.all_sprites.update(dt)
         self.display_surface.fill(('black'))
--- code/player.py
+++ code/player.py
@@ -1,16 +1,21 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((48, 56))
         self.image.fill('gray')
+
+        #rects
         self.rect = self.image.get_frect(topleft=pos)
+        self.old_rect = self.rect.copy()
 
         #movement
         self.direction = vector()
         self.speed = 250
-
+        #collision
+        self.collision_sprites = collision_sprites
+        
     def input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector(0, 0)
@@ -27,7 +32,25 @@ class Player(pygame.sprite.Sprite):
         self.direction = input_vector.normalize() if input_vector else input_vector
        
     def move(self, dt):
-        self.rect.topleft += self.direction * self.speed * dt
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.rect.y == self.direction.y * self.speed * dt
+        self.collision('vertical')
+
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis =="horizontal":
+                    #left
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                else:
+                    pass
+
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(dt)
