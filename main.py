380f61d333791bd86c4c9040e16df4fc4475f9b4
jon-jc <jonathancho.jc@gmail.com>
2024-04-30 17:30:10 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Souls Like Platformer')
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
