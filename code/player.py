368b1176b804e292bb36b83c449fac1450f4de9c
jon-jc <jonathancho.jc@gmail.com>
2024-05-03 12:59:47 -0700

    Feature: Implement Wall Jump Mechanics with Timer Control

--- code/player.py
+++ code/player.py
@@ -1,4 +1,5 @@
 from settings import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
@@ -12,31 +13,37 @@ class Player(pygame.sprite.Sprite):
 
         #movement
         self.direction = vector()
-        self.speed = 250
-        self.gravity = 1000
+        self.speed = 200
+        self.gravity = 1300
         self.jump = False
         self.jump_height = 500
         #collision
         self.collision_sprites = collision_sprites
         self.on_surface = {'floor': False, 'left': False, 'right': False}
 
-        self.display_surface = pygame.display.get_surface()
+        #timer
+        self.timer = {
+            'wall jump': Timer(200),
+        }
         
     def input(self):
         keys = pygame.key.get_pressed()
-        input_vector = vector(0, 0)
+        input_vector = vector(0,0)
 
-       
-        if keys[pygame.K_a]:
-            input_vector.x = -1
-        if keys[pygame.K_d]:
-            input_vector.x = 1
+        if not self.timer['wall jump'].active:
+            
+            if keys[pygame.K_a]:
+                input_vector.x = -1
+            if keys[pygame.K_d]:
+                input_vector.x = 1
+
+                
+            self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
         
-        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
-
         if keys[pygame.K_SPACE]:
             self.jump = True
-       
+            
+            
     def move(self, dt):
         #horizontal
         self.rect.x += self.direction.x * self.speed * dt
@@ -50,55 +57,65 @@ class Player(pygame.sprite.Sprite):
             self.direction.y += self.gravity / 2 * dt
             self.rect.y += self.direction.y * dt
             self.direction.y += self.gravity / 2 * dt
-            self.collision('vertical')
-           
-            if self.jump:
-                if self.on_surface['floor']:
-                    self.direction.y = -self.jump_height
-                elif any((self.on_surface['left'], self.on_surface['right'])):
-                    self.direction.y = -self.jump_height
-                    self.direct.x = 1 if self.on_surface['left'] else -1
-                self.jump = False
+            
+        
+        
+        if self.jump:
+            if self.on_surface['floor']:
+                self.direction.y = -self.jump_height
+            elif any((self.on_surface['left'], self.on_surface['right'])):
+                self.timer['wall jump'].activate()
+                self.direction.y = -self.jump_height
+                self.direction.x = 1 if self.on_surface['left'] else -1
+            self.jump = False
+
+        self.collision('vertical')
+        
+        
+        
+       
+        
+        
+
+        
+        
+        
             
             
     def check_contact(self):
         floor_rect = pygame.Rect(self.rect.bottomleft,(self.rect.width,2))
+        right_rect = pygame.Rect(self.rect.topright + vector(0,self.rect.height / 4),(2,self.rect.height / 2))
+        left_rect  = pygame.Rect(self.rect.topleft + vector(-2,self.rect.height / 4), (2,self.rect.height / 2))
 
-        right_rect = pygame.Rect(self.rect.topright + vector(0, self.rect.height / 4),(2,self.rect.height // 2))
-        left_rect = pygame.Rect(self.rect.topleft + vector(-2, self.rect.height / 4), (2, self.rect.height / 2))
+        
         collide_rects = [sprite.rect for sprite in self.collision_sprites]
-
-    
-
-        #collide with floor
         self.on_surface['floor'] = True if floor_rect.collidelist(collide_rects) >= 0 else False
         self.on_surface['right'] = True if right_rect.collidelist(collide_rects) >= 0 else False
         self.on_surface['left'] = True if left_rect.collidelist(collide_rects) >= 0 else False
-      
+    
 
     def collision(self, axis):
         for sprite in self.collision_sprites:
             if sprite.rect.colliderect(self.rect):
-                if axis =="horizontal":
-                    #left
+                if axis == 'horizontal':
                     if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
                         self.rect.left = sprite.rect.right
-
                     if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
                         self.rect.right = sprite.rect.left
                 else:
-                    #top
                     if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
                         self.rect.top = sprite.rect.bottom
-
-                    #bottom
                     if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
                         self.rect.bottom = sprite.rect.top
-                    self.direction.y = 0
+                        
                 
+    def update_timers(self):
+        for timer in self.timer.values():
+            timer.update()
 
     def update(self, dt):
         self.old_rect = self.rect.copy()
+        self.update_timers
         self.input()
         self.move(dt)
         self.check_contact()
