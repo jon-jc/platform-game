c3b70909871b95515647a95b904dc1dae4ec83d3
jon-jc <jonathancho.jc@gmail.com>
2024-05-05 15:56:09 -0700

    Fix bug in Level class and update player hitbox position

--- code/tempCodeRunnerFile.py
+++ code/tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
-                        self.rect.bottom = sprite.rect.top
-                    self.direction.y = 0
+
+                    sys.exit()
