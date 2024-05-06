ed28c11eee20d6e78dc49c742e6be4d7e3384132
jon-jc <jonathancho.jc@gmail.com>
2024-05-06 11:13:23 -0700

    Implement Enhanced Graphics and Asset Management System

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from level import Level
 from pytmx.util_pygame import load_pygame
 from os.path import join
+from support import *
 
 class Game:
     def __init__(self):
@@ -9,11 +10,18 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Souls Like Platformer')
         self.clock = pygame.time.Clock()
+        self.import_assets()
 
         self.tmx_maps = {0: load_pygame(join('.', 'data', 'levels', 'omni.tmx'))}
-        self.current_stage = Level(self.tmx_maps[0] )
+        self.current_stage = Level(self.tmx_maps[0], self.level_frames)
 
-    
+    def import_assets(self):
+        self.level_frames = {
+            'flag': import_folder('.', 'graphics', 'flag'),
+            'saw': import_folder('.', 'graphics', 'enemies', 'saw', 'animation'),
+            'floor_spike': import_folder('.', 'graphics', 'enemies', 'floor_spikes'),
+            
+        }
 
 
     def run(self):
